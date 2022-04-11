d084d9eba5657b5faeb1d35b599ef55880d567d3
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-04-11 17:54:24 -0300

    Update InputPrintVarStringInt.py

---
+++ AE2/InputPrintVarStringInt.py
@@ -0,0 +1,10 @@
+numero = 5
+numero = numero + 5
+print("el total es:",  numero)
+
+print("Ingrese su nombre:")
+nombre = input() # permite leer información del teclado
+print("Su nombre es:", nombre)
+
+apellido = input("Ingrese su apellido:")
+print("Su nombre completo es:", nombre, apellido)
