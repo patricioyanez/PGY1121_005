d057120335de0875b9dff08f17694e09074b536d
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-06-14 14:00:46 -0400

    Create ejercicioFuncionMenu4.py

---
+++ EA4/ejercicioFuncionMenu4.py
@@ -0,0 +1,38 @@
+# 1.- DEFINIR VARIABLES
+num1 = 0
+num2 = 0
+opcion = "0"
+listaDeOpciones = ["1","2","3","4","5"]
+# 2.- SOLICITAR LOS DATOS
+print("\n ********* Calculadora ********")
+while opcion != "5":
+    print("==== Menu principal ====")
+    print("1.- Sumar")
+    print("2.- Restar")
+    print("3.- Dividir")
+    print("4.- Multiplicar")
+    print("5.- Salir")
+    opcion = input("Ingrese una opción:")
+
+    if opcion not in listaDeOpciones:
+        input("La opción no es válida")
+        input("Presione enter para continuar")
+    elif opcion == "5":
+        print("Adios!!!")
+    else:
+        num1 = int(input("Ingrese valor 1:")) 
+        num2 = int(input("Ingrese valor 2:"))
+        resultado = None
+        if opcion == "1":
+            resultado = sumar(num1, num2)
+        elif opcion == "2":
+            resultado = restar(num1, num2)
+        elif opcion == "3":
+            resultado = dividir(num1, num2)
+        elif opcion == "4":
+            resultado = multiplicar(num1, num2)
+
+        if resultado != None:
+            print(resultado)
+    
+
